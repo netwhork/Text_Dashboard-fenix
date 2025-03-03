62b2b388ed868f17e976d8c76f1b5e097c75523e
gb soft <145601452+netwhork@users.noreply.github.com>
2025-03-03 16:19:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { pref } = require("./test.js");
 const { initDatabase, getAllEmpresas } = require("./database.js");
 
 const app = express();
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 
