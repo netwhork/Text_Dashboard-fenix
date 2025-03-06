0619013a43c16163b958286434fb6e949bdd5d10
gb soft <145601452+netwhork@users.noreply.github.com>
2025-03-06 17:06:26 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const { chromium } = require("playwright");
 async function pref(empresaId) {
   try {
     const browser = await chromium.launch({
-      headless: false,
+      headless: true,
       slowMo: 50,
     });
 
